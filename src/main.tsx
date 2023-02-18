6ee8bf9081018fb49aab781c58783f7bc1eadefd
Mark <mark@vdm.id.au>
2023-02-19 00:40:13 +1100

    Initialize feature/65

---
+++ src/main.tsx
@@ -0,0 +1,43 @@
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './api/ReactQueryClient';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { PageLoading } from '@ui/utils/PageLoading';
+
+/* Code split theme page */
+const ThemePage = lazy(async () => await import('./pages/ThemePage'));
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />
+    },
+    {
+      path: '/theme',
+      element: (
+        <Suspense fallback={<PageLoading />}>
+          <ThemePage />
+        </Suspense>
+      )
+    },
+    {
+      path: '/test',
+      element: <PageLoading />
+    }
+  ],
+  { basename: `${import.meta.env.BASE_URL}` }
+);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
